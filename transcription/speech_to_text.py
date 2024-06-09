ad0ba8d3f6c5dd2fafebcad858db7014f4c7b07c
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-06-09 07:58:58 +0530

    added complete basic transcription feature serving in a flask server along with some testing files

--- transcription/speech_to_text.py
+++
@@ -1,7 +0,0 @@
-import whisper
-
-model = whisper.load_model("base")
-result = model.transcribe("test_audio.mp3", fp16=False)
-
-with open("transcribed_test_audio.txt", "w") as f:
-    f.write(result['text'])
