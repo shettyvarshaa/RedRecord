6fe3324174061ffe1b947614b3c0ab51b1b46f73
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-08-27 00:08:34 +0530

    Refactor

--- library/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.add-subject').addEventListener('click', function() {
-    var newSubject = prompt('Please enter the new subject:');
-    if (newSubject) {
-        alert('You added a new subject: ' + newSubject);
-    }
-});
